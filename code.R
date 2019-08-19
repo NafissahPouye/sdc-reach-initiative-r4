86dc89250d2d87eb6970e5f40b3bf6d971371c57
Nafissah Pouye <Nafissah Pouye>
2019-08-19 02:23:01 +0000

    initial commit

---
+++ code.R
@@ -0,0 +1,32 @@
+library(readxl)      #for excel, csv sheets manipulation
+library(sdcMicro)    #sdcMicro package with functions for the SDC process 
+library(tidyverse)   #optional #for data cleaning
+
+#Import data
+setwd("C:/Users/LENOVO T46OS/Desktop/sdc-reach-initiative-r4")
+data <- read_excel("data.xlsx")
+#Resource1 - Main Frame
+#Comments
+#houses numbers, ids
+selectedKeyVars <- c('facility_name',	'gender',	'position',
+                     'facility_oblast',	'facility_raion',
+                     'facility_settlement', 'adm1NameLat',
+                     'adm2NameLat',	'adm4NameLat',	'cva_area'
+                     )
+
+#Convert variables into factors
+cols =  c('school_level',	'facility_name',	
+          'gender',	'position', 'facility_raion',
+          'facility_settlement')
+data[,cols] <- lapply(data[,cols], factor)
+
+#Convert the sub file into dataframe
+#subVars <- c(selectedKeyVars, weightVars)
+fileRes<-data[,selectedKeyVars]
+fileRes <- as.data.frame(fileRes)
+objSDC <- createSdcObj(dat = fileRes, 
+                       keyVars = selectedKeyVars
+                       )
+
+print(objSDC, "risk")
+report(objSDC, filename ="index",internal = T, verbose = TRUE) 
